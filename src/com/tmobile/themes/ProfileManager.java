e54df0a358cc07ee89fe451b5019cb15b17b72e7
Ed Carrigan <edward.carrigan@t-mobile.com>
2010-01-27 16:46:29 -0800

    Added silent rintone uri
    CR: Josh

--- src/com/tmobile/themes/ProfileManager.java
+++ src/com/tmobile/themes/ProfileManager.java
@@ -1,5 +1,7 @@
 package com.tmobile.themes;
 
+import android.net.Uri;
+
 public interface ProfileManager {
 
     public static final String PERMISSION_CHANGE_PROFILE = "com.tmobile.permission.CHANGE_PROFILE";
@@ -7,5 +9,7 @@ public interface ProfileManager {
     public static final String ACTION_CHANGE_PROFILE = "com.tmobile.intent.action.CHANGE_PROFILE";
 
     public static final String ACTION_PROFILE_CHANGED = "com.tmobile.intent.action.PROFILE_CHANGED";
+    
+    public static final Uri SILENT_RINGTONE_URI = Uri.parse("content://com.tmobile.thememanager/ringtone/silent");
 
 }
