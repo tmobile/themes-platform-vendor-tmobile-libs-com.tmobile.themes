c9fb5a7121b614a0ab2d1efe9e0462b3061cbd6b
Ed Carrigan <edward.carrigan@t-mobile.com>
2010-07-23 14:25:59 -0700

    added new exit_restricted_profile permission/action constants

--- src/com/tmobile/themes/ProfileManager.java
+++ src/com/tmobile/themes/ProfileManager.java
@@ -48,7 +48,11 @@ public interface ProfileManager {
      */
     public static final String PERMISSION_CHANGE_PROFILE = "com.tmobile.permission.CHANGE_PROFILE";
 
+    public static final String PERMISSION_EXIT_RESTRICTED_PROFILE = "com.tmobile.permission.EXIT_RESTRICTED_PROFILE";
+
     public static final String ACTION_CHANGE_PROFILE = "com.tmobile.intent.action.CHANGE_PROFILE";
+
+    public static final String ACTION_EXIT_RESTRICTED_PROFILE = "com.tmobile.intent.action.EXIT_RESTRICTED_PROFILE";
 
     public static final String ACTION_PROFILE_CHANGED = "com.tmobile.intent.action.PROFILE_CHANGED";
     
